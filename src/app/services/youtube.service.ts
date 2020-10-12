434ca3166d778e38e54a194158cf989813a02b74
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-12 14:58:20 -0500

    292 servicios para obtener videos de youtube

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyB61EdK82CfNndrh7bWqngT8hqm3hFRaog';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) {}
+
+  getVideos() {
+
+    const url = `${ this.youtubeUrl }/playlistItems`;
+
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('maxResults', '10')
+    .set('playlistId', this.playlist)
+    .set('key', this.apikey);
+
+    return this.http.get( url, { params });
+  }
+
 }
