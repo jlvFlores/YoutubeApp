9549c2f0226b01acb629e887a1aee9ff304d6cf8
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-12 15:46:54 -0500

    293 estraer datos y el tipado de la respuesta

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +25,15 @@ export class YoutubeService {
     .set('playlistId', this.playlist)
     .set('key', this.apikey);
 
-    return this.http.get( url, { params });
+    return this.http.get<YoutubeResponse>( url, { params })
+    .pipe(
+
+      map( resp => {
+        this.nextPageToken = resp.nextPageToken;
+        return resp.items;
+      }),
+
+      map( items => items.map( video => video.snippet ) );
   }
 
 }
