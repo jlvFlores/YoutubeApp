26df86e60d278c60efa6ac95d47cdbb8e2ca76a2
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-12 16:43:46 -0500

    295 Mostrar un model con el video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from 'src/app/models/youtube.models';
 import { YoutubeService } from '../../services/youtube.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -23,4 +24,25 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  mostrarVideo( video: Video ) {
+    console.log( video );
+    Swal.fire({
+      html: `
+      <h4> ${ video.title } </h4>
+      <hr>
+      <iframe width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/${ video.resourceId.videoId }"
+        frameborder="0"
+        allow="accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture"
+        allowfullscreen>
+      </iframe>
+      `
+    });
+  }
 }
